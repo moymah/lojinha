71cc8375e4b62567654b81ce17e7ab9ec0c6b13c
Marisa <marisa.alves@77sol.com.br>
2021-08-02 11:36:57 -0300

    feat: add api services

---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+export const api = axios.create({
+  baseURL: process.env.REACT_APP_LOCAL_API_URL,
+  headers: {
+    common: {
+      'Content-Type': 'application/json'
+    }
+  }
+})
