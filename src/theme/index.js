3686fad3a8ef90f767dfe15c14924399b19d7696
Marisa <marisa.alves@77sol.com.br>
2021-08-02 11:36:45 -0300

    feat: add theme

---
+++ src/theme/index.js
@@ -0,0 +1,46 @@
+const theme = {
+  colors: {
+    primary: {
+      100: '#FFF1EB',
+      lighter: '#FF621F',
+      main: '#FF5003',
+      darker: '#F54900'
+    },
+    secondary: {
+      100: '#FFF6EB',
+      lighter: '#FF9A1F',
+      main: '#FF8E00',
+      darker: '#F58700'
+    },
+    grayscale: {
+      100: '#F5F5F5',
+      200: '#D6D6D6',
+      lighter: '#525252',
+      main: '#333333',
+      darker: '#0A0A0A'
+    },
+    background: {
+      darker: '#002347',
+      lighter: '#5CADFF'
+    },
+    white: '#FFFFFF',
+    black: '#000000'
+  },
+  effects: {
+    boxShadow: '1px 3px 8px 0px #0f75f53b'
+  },
+  borderRadius: {
+    small: '8px',
+    medium: '16px',
+    large: '24px',
+    rounded: '50%'
+  },
+  fontSizes: {
+    xsmall: '8px',
+    small: '12px',
+    medium: '16px',
+    large: '24px'
+  }
+}
+
+export default theme
